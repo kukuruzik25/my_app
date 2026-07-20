e3fbe043861435fe9550a34bae3925568a53237b
Tsyb Darya Dmitrievna <kukuruzik25@mail.ru>
2026-07-20 19:10:38 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
-final DateTime startDate = DateTime(2026, 7, 13);
+final DateTime startDate = DateTime(2026, 7, 8);
 final DateTime eventDate = DateTime(2026, 8, 4);
 const String eventTitle = 'Тока тока тока';
